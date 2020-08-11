cd9408d367c5fe43624cd40917afe1419b7ff897
Wirzka <60296453+wirzka@users.noreply.github.com>
2020-08-11 11:31:11 +0200

    Update act.py

--- act.py
+++ act.py
@@ -1,11 +1,24 @@
 from rss import RSS
+import os
 
-u = RSS('https://csirt.gov.it/data/indexer/rss')
+url = "https://csirt.gov.it/data/indexer/rss"
+xml = "csirtita.xml"
+
+u = RSS(url, xml)
 
 def main():
+
+	# retrieve the rss feed and bring it on local as xml file
 	u.RSS2XML()
+
+	# take the title from the item inside the XML file
 	u.XMLParser()
+
+	# save the output to a json file named: csirtita_<TodayDate>.json
 	u.Cont2File()
+
+	# cleaning
+	os.remove(xml)
 
 if __name__ == "__main__":
 	main()
