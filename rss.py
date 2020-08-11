57d7ea95fab738d04d793a8e6bc0df6c93c0fa7d
Wirzka <60296453+wirzka@users.noreply.github.com>
2020-08-11 11:30:53 +0200

    Update rss.py

--- rss.py
+++ rss.py
@@ -7,30 +7,48 @@ import csv
 
 class RSS():
 
-    def __init__(self, url):
+    def __init__(self, url, xml):
+        # setting up
         self._url = url
-        self._xml = 'csirtita.xml'
+        self._xml = xml
         self._json = 'csirtita_'
+        # list to store output
         self.l = []
 
     def RSS2XML(self):
-    	res = requests.get(self._url)
 
+        # taking the RSS feed
+    	res = requests.get(self._url)
+        # putting the response on file
     	with open(self._xml, 'wb') as f:
     		f.write(res.content)
 
     def XMLParser(self):
-        it = ET.parse(self._xml)
-        root = it.getroot()
+        # reading the xml file
+        xmlParsed = ET.parse(self._xml)
+
+        # retrieving the root
+        root = xmlParsed.getroot()
+
+        # iterating the titles
         for item in root.iter('title'):
-            testo = item.text
-            if len(testo.splitlines()) == 2:
-                self.l.append(testo.splitlines())
-        # pprint.pprint(json.dumps(self.l))
+            title = item.text
+            '''
+                The CSIRT alert is structured as follow:
+                ['Title describing the alert', (ALXX/XXXXXX/CSIRT-ITA)']
+                So we are taking only the titles with lenght of 2
+            '''
+            if len(title.splitlines()) == 2:
+                # adding the line on the list for later
+                self.l.append(title.splitlines())
+                #  printing the line
+                pprint.pprint(title.splitlines())
 
     def Cont2File(self):
+        # retrieve today date and formatting it
         today = datetime.today().strftime('%Y-%m-%d')
+        # backing the filename
         filename = self._json + today + '.json'
-        # print(filename)
+        # writing JSON data on the file
         with open(filename, 'w') as oF:
              oF.write(json.dumps(self.l))
